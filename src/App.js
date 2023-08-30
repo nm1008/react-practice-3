4e8b8945a616826545e3f07404e4ca11d06901f6
nm1008 <mallari08nikko@yahoo.com>
2023-08-31 01:52:49 +0800

    react practice 3 tip calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [friendTip, setFriendTip] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TipCalculator
+        bill={bill}
+        setBill={setBill}
+        setTip={setTip}
+        setFriendTip={setFriendTip}
+      />
+      <Total bill={bill} tip={tip} friendTip={friendTip} />
     </div>
+  );
+}
+
+function TipCalculator({ bill, setBill, setTip, setFriendTip }) {
+  return (
+    <>
+      <div>
+        <label>How Much was the bill? </label>
+        <input
+          value={bill}
+          type="number"
+          placeholder="Amount..."
+          onChange={(e) => setBill(Number(e.target.value))}
+        />
+      </div>
+
+      <div>
+        <label>How did you like the service? </label>
+        <select onChange={(e) => setTip(Number(e.target.value))}>
+          <option value={0}>Dissatisfied (0%)</option>
+          <option value={5}>It was okay (5%)</option>
+          <option value={10}>It was good(10%)</option>
+          <option value={20}>Absolutely Amazing!(20%)</option>
+        </select>
+      </div>
+
+      <div>
+        <label>How did your friend like the service? </label>
+        <select onChange={(e) => setFriendTip(Number(e.target.value))}>
+          <option value={0}>Dissatisfied (0%)</option>
+          <option value={5}>It was okay (5%)</option>
+          <option value={10}>It was good(10%)</option>
+          <option value={20}>Absolutely Amazing!(20%)</option>
+        </select>
+      </div>
+    </>
+  );
+}
+
+function Total({ bill, tip, friendTip }) {
+  const totalTip = (tip + friendTip) / 2;
+  const totalAmount = bill + totalTip;
+
+  return (
+    <>
+      <h1>
+        You pay ${totalAmount} (${bill} + {totalTip} tip)
+      </h1>
+    </>
   );
 }
 
